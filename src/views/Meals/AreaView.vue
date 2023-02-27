ad59a057453089c5daf783d32fe37a102d45ba1b
kiboma <omayioben@gmail.com>
2023-02-27 17:49:36 +0300

    Fetch area API

--- src/views/Meals/AreaView.vue
+++ src/views/Meals/AreaView.vue
@@ -1,10 +1,26 @@
 <template>
     <h1>Areas</h1>
+
+    <div v-for="area in areas.meals" :key="area.strArea">
+        {{ area.strArea}}
+    
+    </div>
   
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            areas: [],
+        }
+    },
+    mounted(){
+        fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+        .then(res => res.json())
+        .then(data => this.areas = data)
+        .catch(err => console.log(err.message))
+    }
 
 }
 </script>
