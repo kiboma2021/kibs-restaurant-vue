17a8604ee2a96d4246bd7a1bf0870f96f31a665c
kiboma <omayioben@gmail.com>
2023-02-27 17:41:57 +0300

    Add FoodCategory Component

---
+++ src/views/Meals/FoodCategoryView.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <h1>Food Categories</h1>
+    <hr>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody v-for="category in categories.categories" :key="category.idCategory">
+                <tr>
+                    <td>{{ category.strCategory }}</td>
+                    <td>{{ category.strCategoryDescription }}</td>
+                </tr>
+
+            </tbody>
+
+        </table>
+        
+
+    </div>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            categories: []
+        }
+    },
+    mounted(){
+      fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+        .then(res => res.json())
+        .then(data => this.categories = data)
+        .catch(err => console.log(err.message))
+    }
+
+}
+</script>
+
+<style>
+
+</style>
